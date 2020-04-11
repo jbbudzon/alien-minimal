740a9ed391a944ba4854e9efd1e3b3657cbbed26
jbbudzon <46833444+jbbudzon@users.noreply.github.com>
2020-04-11 10:22:19 -0400

    pers tmux tweaks
    not for upstream

--- modules/bgtmux.zsh
+++ modules/bgtmux.zsh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 am_tmux_st(){
-  if [[ -n "${TMUX}" || "$(tmux ls 2>/dev/null | wc -l)" -eq 0 ]]; then
+  if [[ -n "${TMUX}" || "$(ts | wc -l)" -eq 0 ]]; then
     echo -ne ""
   else
-    echo -n " %F{$AM_TMUX_COLOR}${AM_TMUX_SYM}%f"
+    echo -n " %F{$AM_TMUX_COLOR}"
+    for (( iter = 0 ; iter < $(ts | wc -l) ; ++iter )); do
+      echo -n "${AM_TMUX_SYM}"
+    done
+    echo -n "%f"
   fi
 }
