e6201b7f8d9944e7d48336c75cb120dcf8f35c27
jbbudzon <46833444+jbbudzon@users.noreply.github.com>
2020-10-10 22:42:21 -0400

    perf fix for pers bg tweaks

--- modules/bgtmux.zsh
+++ modules/bgtmux.zsh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 am_tmux_st(){
-  if [[ -n "${TMUX}" || "$(ts | wc -l)" -eq 0 ]]; then
+  # warning ts names cannot contain whitespace
+  VAL=${#$(ts)[@]}
+  if [[ -n "${TMUX}" || "${VAL}" -eq 0 ]]; then
     echo -ne ""
   else
     echo -n "%F{$AM_TMUX_COLOR}"
-    for (( iter = 0 ; iter < $(ts | wc -l) ; ++iter )); do
+    for (( iter = 0 ; iter < ${VAL} ; ++iter )); do
       echo -n "${AM_TMUX_SYM}"
     done
     echo -n "%f "
